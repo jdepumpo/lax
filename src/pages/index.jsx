ea74a55a0976298b823ce553523116974bde1899
jdepumpo <joe@depumpo.com>
2018-09-22 14:04:25 -0400

    initial

---
+++ src/pages/index.jsx
@@ -0,0 +1,226 @@
+/* global tw */
+import React from 'react';
+import styled from 'react-emotion';
+import { Parallax, ParallaxLayer } from 'react-spring';
+import 'typeface-cantata-one';
+import 'typeface-open-sans';
+
+import SEO from '../components/SEO';
+import SVG from '../components/SVG';
+import ProjectCard from '../components/ProjectCard';
+import { rotate, UpDown, UpDownWide, waveAnimation } from '../styles/animations';
+import { hidden } from '../styles/utils';
+import { colors } from '../../tailwind';
+import triangle from '../images/triangle.svg';
+import avatar from '../images/avatar.jpg';
+import '../styles/global';
+
+const Divider = styled(ParallaxLayer)`
+  ${tw('absolute w-full h-full')};
+  background: ${props => props.bg};
+  svg {
+    fill: ${props => props.fill};
+  }
+  clip-path: ${props => props.clipPath};
+`;
+
+const DividerMiddle = styled(Divider)`
+  clip-path: polygon(0 15%, 100% 25%, 100% 85%, 0 75%);
+`;
+
+const Content = styled(ParallaxLayer)`
+  ${tw('p-6 md:p-12 lg:p-24 justify-center items-center flex z-50')};
+`;
+
+const Hero = styled.div`
+  ${tw('w-full xl:w-2/3 border-solid border-purple rounded-lg p-6')};
+`;
+
+const Inner = styled.div`
+  ${tw('w-full xxl:w-2/3 text-center lg:text-left')};
+`;
+
+const BigTitle = styled.h1`
+  ${tw('text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide')};
+  text-shadow: 0 5px 35px rgba(255, 255, 255, 0.15);
+`;
+
+const Title = styled.h1`
+  ${tw('text-4xl lg:text-4xl font-serif text-white mb-8 tracking-wide relative inline-block')};
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  &:before {
+    content: '';
+    width: 40px;
+    height: 40px;
+    background: url(${triangle});
+    position: absolute;
+    background-size: 40px;
+    animation: ${rotate} 4s linear infinite;
+    left: -60px;
+    top: 5px;
+  }
+`;
+
+const Subtitle = styled.p`
+  ${tw('text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4')};
+  text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
+`;
+
+const ProjectsWrapper = styled.div`
+  ${tw('flex flex-wrap justify-between mt-8')};
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(2, 1fr);
+  @media (max-width: 1200px) {
+    grid-gap: 3rem;
+  }
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem;
+  }
+`;
+
+const WaveWrapper = styled.div`
+  ${tw('absolute pin-b w-full')};
+  transform: matrix(1, 0, 0, -1, 0, 0);
+`;
+
+const InnerWave = styled.div`
+  ${tw('relative h-full')};
+  svg {
+    width: 100%;
+    height: 40vh;
+  }
+`;
+
+const GoDown = styled.div`
+  ${tw('absolute pin-b h-24')};
+  svg {
+    transform: rotate(180deg);
+  }
+`;
+
+const AboutHero = styled.div`
+  ${tw('flex flex-col lg:flex-row items-center mt-8')};
+`;
+
+const Avatar = styled.img`
+  ${tw('rounded-full w-32 xl:w-48 shadow-lg h-full')};
+`;
+
+const AboutSub = styled.span`
+  ${tw('text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl')};
+`;
+
+const AboutDesc = styled.p`
+  ${tw('text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify')};
+`;
+
+const ContactText = styled.p`
+  ${tw('text-grey-light font-sans text-xl md:text-2xl lg:text-3xl')};
+  a {
+    color: #e07628;
+    text-decoration: none;
+  }
+`;
+
+const Footer = styled.footer`
+  ${tw('text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg')};
+  a {
+    color: #e07628;
+    text-decoration: none;
+  }
+`;
+
+const Index = () => (
+  <React.Fragment>
+    <SEO />
+    <Parallax pages={4}>
+    <Divider fill="#23262b" speed={0.2} offset={0}>
+        <WaveWrapper>
+          <InnerWave>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
+              <path className={waveAnimation}>
+                <animate
+                  attributeName="d"
+                  values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
+                  repeatCount="indefinite"
+                  dur="30s"
+                />
+              </path>
+            </svg>
+          </InnerWave>
+        </WaveWrapper>
+      </Divider>
+      <Divider bg="#23262b" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={.8} />
+      <Content speed={0.4} offset={0}>
+        <Hero>
+          <BigTitle>
+            jfd3.
+          </BigTitle>
+          <Subtitle>The big brown fox jumps over the log.</Subtitle>
+        </Hero>
+        <GoDown>
+        <SVG icon="arrowUp" className={hidden} width={8} fill={colors["purple-light"]}/>
+        </GoDown>
+      </Content>
+      <DividerMiddle
+        bg="linear-gradient(to right, rebeccaPurple 0%, Purple 100%)"
+        speed={-0.2}
+        offset={.6}
+        factor={1}
+      />
+      <Content speed={0.4} offset={.5} factor={2}>
+        <Inner>
+          <Title>Projects</Title>
+          <ProjectsWrapper>
+            <ProjectCard
+              title="Freiheit"
+              link="https://www.behance.net/gallery/58937147/Freiheit"
+              bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+            >
+              This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            </ProjectCard>
+            <ProjectCard
+              title="Harry Potter"
+              link="https://www.behance.net/gallery/52915793/Harry-Potter"
+              bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+            >
+              I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            </ProjectCard>
+            <ProjectCard
+              title="Tomb Raider"
+              link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+              bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+            >
+              Recreation of a Tomb Raider Wallpaper (Fan Art)
+            </ProjectCard>
+            <ProjectCard
+              title="Eagle"
+              link="https://www.behance.net/gallery/38068151/Eagle"
+              bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
+            >
+              A fantasy image manipulation relocating the habitat of wild animals.
+            </ProjectCard>
+          </ProjectsWrapper>
+        </Inner>
+      </Content>
+      <Content speed={0.4} offset={2.5}>
+        <Inner>
+          <Title>Get in touch</Title>
+          <ContactText>
+            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
+            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
+            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+          </ContactText>
+        </Inner>
+        <Footer>
+          &copy; 2018 by Gatsby Starter Portfolio Cara.{' '}
+          <a href="https://github.com/LeKoArts/gatsby-starter-portfolio-cara">Github Repository</a>.
+        </Footer>
+      </Content>
+    </Parallax>
+  </React.Fragment>
+);
+
+export default Index;
